13069aedd0ec6e177700c8c961b3fefe476a13e9
Ville Verkkonen <villeverkkonen@gmail.com>
2016-12-03 18:06:38 +0200

    etusivulta linkki kauppalistaan ja tyhjät luokat Kauppalista ja KauppalistaRepository

---
+++ src/main/java/kauppalista/domain/Kauppalista.java
@@ -0,0 +1,10 @@
+
+package kauppalista.domain;
+
+import javax.persistence.Entity;
+import org.springframework.data.jpa.domain.AbstractPersistable;
+
+@Entity
+public class Kauppalista extends AbstractPersistable<Long> {
+    
+}
---
+++ src/main/java/kauppalista/repository/KauppalistaRepository.java
@@ -0,0 +1,10 @@
+
+package kauppalista.repository;
+
+import kauppalista.domain.Kauppalista;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface KauppalistaRepository extends JpaRepository<Kauppalista, Long> {
+    
+}

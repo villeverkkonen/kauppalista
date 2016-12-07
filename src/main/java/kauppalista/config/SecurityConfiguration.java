3b3fb5da620ab6014945d564d18b261e78ec9772
Ville Verkkonen <villeverkkonen@gmail.com>
2016-12-08 00:15:25 +0200

    nyt uloskirjautuminen ohjaa kaikille avoimelle etusivulle

--- src/main/java/kauppalista/config/SecurityConfiguration.java
+++ src/main/java/kauppalista/config/SecurityConfiguration.java
@@ -31,7 +31,8 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .permitAll()
                 .and()
                 .logout()
-                .permitAll();
+                .logoutUrl("/logout")
+                .logoutSuccessUrl("/etusivu");
     }
 
     @Autowired

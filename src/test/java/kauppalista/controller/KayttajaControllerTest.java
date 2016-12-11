4f910d5fd33124215da3ac47dab58eb545b412a3
Antti Nuortimo <antti.nuortimo@gmail.com>
2016-12-11 19:09:06 +0200

    Testi: etusivuOnOlemassaJaSiihenLiittyyAttribuuttiKayttajat()
    Tämä on Kauppalistan ensimmäinen testi.
    
    Kaikki testit menevät läpi.
    
    Tests run: 1, Failures: 0, Errors: 0, Skipped: 0
    
    	new file:   src/test/java/kauppalista/controller/KayttajaControllerTest.java

---
+++ src/test/java/kauppalista/controller/KayttajaControllerTest.java
@@ -0,0 +1,38 @@
+package kauppalista.controller;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class KayttajaControllerTest {
+
+    @Autowired
+    private WebApplicationContext context;
+
+    private MockMvc mockMvc;
+
+    @Before
+    public void setUp() throws Exception {
+        mockMvc = MockMvcBuilders.webAppContextSetup(context).build();
+    }
+
+    @Test
+    public void etusivuOnOlemassaJaSiihenLiittyyAttribuuttiKayttajat() throws Exception {
+        MvcResult res = this.mockMvc.perform(get("/etusivu"))
+                .andExpect(model().attributeExists("kayttajat"))
+                .andExpect(view().name("etusivu"))
+                .andReturn();
+    }
+}

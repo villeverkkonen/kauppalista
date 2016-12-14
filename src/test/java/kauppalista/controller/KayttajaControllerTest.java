f92ef75550b3de3e930747738a6d729dc6c76e49
Antti Nuortimo <antti.nuortimo@gmail.com>
2016-12-14 19:49:28 +0200

    Testataan, että salasanan pituus on vähintään 8 merkkiä.
    Jos salasanan pituus ei ole vähintään 8 merkkiä, salasanaa ei hyväksytä.
    
    	modified:   src/test/java/kauppalista/controller/KayttajaControllerTest.java

--- src/test/java/kauppalista/controller/KayttajaControllerTest.java
+++ src/test/java/kauppalista/controller/KayttajaControllerTest.java
@@ -126,4 +126,35 @@ public class KayttajaControllerTest {
         assertTrue("Kun on yritetty luoda uusi käyttäjä, jolla on varattu käyttäjänimi, käyttäjien määrä ei saa muuttua.",
                 kayttajienLkmLisaysyrityksenJalkeen == kayttajienLkmLisayksenJalkeen);
     }
+
+    @Test
+    public void eiLyhyttaSalasanaa() throws Exception {
+        MvcResult res = this.mockMvc.perform(get("/etusivu"))
+                .andExpect(model().attributeExists("kayttajat"))
+                .andExpect(view().name("etusivu"))
+                .andReturn();
+
+        List<Kayttaja> kayttajatAlussa = new ArrayList((Collection<Kayttaja>) res.getModelAndView().getModel().get("kayttajat"));
+
+        int kayttajienLkmAlussa = kayttajatAlussa.size();
+
+        String kayttajanimi = "kayttaja" + UUID.randomUUID().toString();
+        String salasana = "abcd01!";
+
+        this.mockMvc.perform(post("/etusivu").param("kayttajanimi", kayttajanimi).param("salasana", salasana))
+                .andExpect(status().is2xxSuccessful())
+                .andReturn();
+
+        MvcResult resLisaysyrityksenJalkeen = this.mockMvc.perform(get("/etusivu"))
+                .andExpect(model().attributeExists("kayttajat"))
+                .andExpect(view().name("etusivu"))
+                .andReturn();
+
+        List<Kayttaja> kayttajatLisaysyrityksenJalkeen
+                = new ArrayList((Collection<Kayttaja>) resLisaysyrityksenJalkeen.getModelAndView().getModel().get("kayttajat"));
+        int kayttajienLkmLisaysyrityksenJalkeen = kayttajatLisaysyrityksenJalkeen.size();
+
+        assertTrue("Kun on yritetty luoda uusi käyttäjä, jolla liian lyhyt salasana, käyttäjätunnusten määrä ei saa muuttua.",
+                kayttajienLkmLisaysyrityksenJalkeen == kayttajienLkmAlussa);
+    }
 }

6f76aaeac7e8c41a0ecc87d7504af9b269fbccb2
Antti Nuortimo <antti.nuortimo@gmail.com>
2016-12-16 01:43:05 +0200

    Poistettu käyttämättömiä importteja.
    	modified:   src/main/java/kauppalista/controller/TuotantoDefaultController.java

--- src/main/java/kauppalista/controller/TuotantoDefaultController.java
+++ src/main/java/kauppalista/controller/TuotantoDefaultController.java
@@ -1,13 +1,7 @@
 package kauppalista.controller;
 
 import javax.annotation.PostConstruct;
-import kauppalista.repository.KauppalistaRepository;
-import kauppalista.repository.KayttajaRepository;
-import kauppalista.repository.TuoteRepository;
-import kauppalista.service.KauppalistaService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Profile;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
